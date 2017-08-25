49b05a914bb0c68476760a9d6932f422f57c5b61
AxeChen <1137366723@qq.com>
2017-08-26 03:24:13 +0800

    CoordinatorLayout隐藏toolbar的效果

---
+++ MaterialDesignSimple/coordinatorlayoutscrollsimple/src/main/java/com/mg/axe/coordinatorlayoutscrollsimple/MainActivity.java
@@ -0,0 +1,16 @@
+package com.mg.axe.coordinatorlayoutscrollsimple;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
