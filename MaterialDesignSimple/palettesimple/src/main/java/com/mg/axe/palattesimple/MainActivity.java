a0f8e188b3daabfb965df02e34db5a631e360f15
AxeChen <1137366723@qq.com>
2017-09-10 22:38:39 +0800

    palette 示例代码

--- MaterialDesignSimple/palettesimple/src/main/java/com/mg/axe/palattesimple/MainActivity.java
+++ MaterialDesignSimple/palettesimple/src/main/java/com/mg/axe/palattesimple/MainActivity.java
@@ -1,8 +1,10 @@
 package com.mg.axe.palattesimple;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by Axe on 2017/9/10.
@@ -13,6 +15,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
+    public void basicUse(View view) {
+        Intent intent = new Intent(MainActivity.this, PalettesBasicActivity.class);
+        startActivity(intent);
+    }
+
+    public void simpleUse(View view) {
+        Intent intent = new Intent(MainActivity.this, TabLayoutViewpagerActivity.class);
+        startActivity(intent);
     }
 }
